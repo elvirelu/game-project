0f675419ab40c85331fc00c87049ec37e657bf11
Lu <e2295539@cmaisonneuve.qc.ca>
2022-05-02 11:43:15 -0400

    Update game2 copycopy.py

--- game2 copycopy.py
+++ game2 copycopy.py
@@ -79,31 +79,33 @@ class Player(pygame.sprite.Sprite):
         self.velocity = Vector2(0, 0)
         self.acceleration = Vector2(0, 0)
         self.direction = True
+        self.attacking = False
+        self.move_frame = 0
+        self.attack_fram = 0
         self._layer = 1
 
     def update(self, delta_time):
         self.move()
-        self.rect.center = (round(self.position.x), round(self.position.y))
-
+        self.run()
+        
     def move(self):
-        self.acceleration = Vector2(0, GRAV)
-        # taken from https://coderslegacy.com/python/pygame-gravity-and-jumping/
+        self.acceleration = Vector2(0, 0.5)
+
         # Returns the current key presses
-        pressed_keys = pygame.key.get_pressed()
+        self.pressed_keys = pygame.key.get_pressed()
 
         # Accelerates the player in the direction of the key press
-        if pressed_keys[K_RIGHT]:
+        if self.pressed_keys[K_RIGHT]:
             self.acceleration.x = 0.3
-        if pressed_keys[K_LEFT]:
+        elif self.pressed_keys[K_LEFT]:
             self.acceleration.x = -0.3
-        if pressed_keys[K_UP]:
-            self.acceleration.y = -0.5
-        
+
+        # calculate the velocity
         self.velocity += self.acceleration
-        self.velocity.x *= 1 - FRIC
+        self.velocity.x *= 0.95
 
         hits = pygame.sprite.spritecollide(self, game.current_scene.ground, False)
-        # check if touch the ground, then stop
+        # check if touch the ground, if touch, disable the velocity of direction y
         if self.velocity.y > 0:
             if hits:
                 if self.rect.bottom > hits[0].rect.top:
@@ -111,11 +113,45 @@ class Player(pygame.sprite.Sprite):
                     self.velocity.y = 0
 
         self.position += self.velocity
-        if self.direction:
-            self.image = pygame.image.load("Player_Sprite_R.png")
-        else:
-            self.image = pygame.image.load("Player_Sprite_L.png")
+        self.rect.center = (round(self.position.x), round(self.position.y))
 
+        # keep player inside the screen
+        if self.position.x > W:
+            self.position.x = 0
+        elif self.position.x < 0:
+            self.position.x = W
+
+    def run(self):
+        # reset move frame
+        if self.move_frame > 6:
+            self.move_frame = 0
+
+        # move player to next frame
+        if self.velocity.x > 0.3:
+            self.image = run_ani_R[self.move_frame]
+            self.direction = True
+        elif self.velocity.x < -0.3:
+            self.image = run_ani_L[self.move_frame]
+            self.direction = False
+        else:
+            if self.direction:
+                self.image = run_ani_R[0]
+            else:
+                self.image = run_ani_L[0]
+        self.move_frame += 1
+        
+    def attack(self):
+        # return to base frame
+        if self.attack_frame > 10:
+            self.attack_frame = 0
+            self.attacking = False
+        # check direction for good display
+        if self.direction:
+            self.image = attack_ani_R[self.attack_frame]
+        else:
+            self.image = attack_ani_L[self.attack_frame]
+        self.attack_frame += 1
+    
 class Enemy(pygame.sprite.Sprite):
     def __init__(self, position):
         super().__init__()
@@ -134,7 +170,6 @@ class Ground(pygame.sprite.Sprite):
 class Game():
     def __init__(self):
         pygame.init()
-        self.running = True
         self.clock = pygame.time.Clock()
         self.sprites = pygame.sprite.LayeredUpdates()
         self.display = pygame.display.set_mode((W, H))
@@ -142,22 +177,25 @@ class Game():
         self.ui_manager = pygame_gui.UIManager((W, H))
         self.current_scene = None
 
-    def run(self):
+    def start(self):
         self.shake = False
         self.current_scene = TitleScene()
         self.offset = (0, 0)
-        while self.running:
+        while True:
             self.tick()
 
     def handle_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                self.running = False
+                pygame.quit()
+            if event.type == KEYDOWN:
+                if event.key == pygame.K_a:
+                    
             self.ui_manager.process_events(event)
             self.current_scene.process_events(event)
 
     def tick(self):
-        delta_time = self.clock.tick(FPS)/1000.0
+        delta_time = self.clock.tick(120)/1000
         self.handle_events()
         self.ui_manager.update(delta_time)
         self.sprites.update(delta_time)
@@ -176,10 +214,8 @@ class Game():
 
 W = 700
 H = 350
-FPS = 120
-FRIC = 0.05
-GRAV = 0.5
 
+# taken from https://coderslegacy.com/python/pygame-rpg-movement-animations/
 # run animation for the right
 run_ani_R = [pygame.image.load("Player_Sprite_R.png"), pygame.image.load("Player_Sprite2_R.png"),
              pygame.image.load("Player_Sprite3_R.png"), pygame.image.load("Player_Sprite4_R.png"),
@@ -192,6 +228,27 @@ run_ani_L = [pygame.image.load("Player_Sprite_L.png"), pygame.image.load("Player
              pygame.image.load("Player_Sprite5_L.png"), pygame.image.load("Player_Sprite6_L.png"),
              pygame.image.load("Player_Sprite_L.png")]
 
+# Attack animation for the RIGHT
+attack_ani_R = [pygame.image.load("Player_Sprite_R.png"), pygame.image.load("Player_Attack_R.png"),
+                pygame.image.load("Player_Attack2_R.png"), pygame.image.load("Player_Attack2_R.png"),
+                pygame.image.load("Player_Attack3_R.png"), pygame.image.load("Player_Attack3_R.png"),
+                pygame.image.load("Player_Attack4_R.png"), pygame.image.load("Player_Attack4_R.png"),
+                pygame.image.load("Player_Attack5_R.png"), pygame.image.load("Player_Attack5_R.png"),
+                pygame.image.load("Player_Sprite_R.png")]
+
+# Attack animation for the LEFT
+attack_ani_L = [pygame.image.load("Player_Sprite_L.png"), pygame.image.load("Player_Attack_L.png"),
+                pygame.image.load("Player_Attack2_L.png"), pygame.image.load("Player_Attack2_L.png"),
+                pygame.image.load("Player_Attack3_L.png"), pygame.image.load("Player_Attack3_L.png"),
+                pygame.image.load("Player_Attack4_L.png"), pygame.image.load("Player_Attack4_L.png"),
+                pygame.image.load("Player_Attack5_L.png"), pygame.image.load("Player_Attack5_L.png"),
+                pygame.image.load("Player_Sprite_L.png")]
+
+# health bar animation
+health_ani = [pygame.image.load("heart0.png"), pygame.image.load("heart.png"),
+              pygame.image.load("heart2.png"), pygame.image.load("heart3.png"),
+              pygame.image.load("heart4.png"), pygame.image.load("heart5.png")]
+
 if __name__ == '__main__':
     game = Game()
-    game.run()
+    game.start()
