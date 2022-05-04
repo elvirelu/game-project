aba419e6974c4e8fb0240f91084355fce3793068
Jing Lu <elvirelu@gmail.com>
2022-05-04 19:24:06 -0400

    reorganise all the files

--- game_final.py
+++ game_final.py
@@ -6,9 +6,12 @@ class Menu:
 
 class Game:
     start_game()
+    level1()
+    level2()
+    level3()
     event_handle()
-    menu()
     update_scene()
+    respawn()
 
 class Player:
     set_pos()
@@ -21,69 +24,73 @@ class Enemy:
     move()
     hit()
     update()
+
+class Healthbar
+class Background
 """
 
+import random
 import pygame
 from pygame import *
 from tkinter import *
 
+# create class Background
 class Background(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.image = pygame.image.load("background.png")
+        self.image = pygame.image.load("background/background.jpg")
         self.rect = self.image.get_rect(topleft=(0, 0))
 
-class Ground(pygame.sprite.Sprite):
-    def __init__(self):
-        super().__init__()
-        self.image = pygame.image.load("ground.png")
-        self.rect = self.image.get_rect(center=(350, 350))
-
+# create class HealthBar
 class HealthBar(pygame.sprite.Sprite):
+    # taken from https://coderslegacy.com/python/pygame-rpg-health-bar/
     # health bar animation
-    health_ani = [pygame.image.load("heart0.png"), pygame.image.load("heart.png"),
-                pygame.image.load("heart2.png"), pygame.image.load("heart3.png"),
-                pygame.image.load("heart4.png"), pygame.image.load("heart5.png")]
+    health_ani = [pygame.image.load("healthbar/heart0.png"), pygame.image.load("healthbar/heart.png"),
+                pygame.image.load("healthbar/heart2.png"), pygame.image.load("healthbar/heart3.png"),
+                pygame.image.load("healthbar/heart4.png"), pygame.image.load("healthbar/heart5.png")]
+
     def __init__(self):
         super().__init__()
-        self.image = pygame.image.load("heart5.png")
+        self.image = pygame.image.load("healthbar/heart5.png")
         self.rect = self.image.get_rect(topleft=(10, 10))
+    
+    # when player's health value changes, change the healthbar picture
+    def update(self):
+        self.image = self.health_ani[game.player.health]
 
+# class Player
 class Player(pygame.sprite.Sprite):
     # taken from https://coderslegacy.com/python/pygame-rpg-movement-animations/
     # move animation for the right
-    move_ani_R = [pygame.image.load("Player_Sprite_R.png"), pygame.image.load("Player_Sprite2_R.png"),
-                pygame.image.load("Player_Sprite3_R.png"), pygame.image.load("Player_Sprite4_R.png"),
-                pygame.image.load("Player_Sprite5_R.png"), pygame.image.load("Player_Sprite6_R.png"),
-                pygame.image.load("Player_Sprite_R.png")]
-
+    move_ani_R = [pygame.image.load("player/Player_Sprite_R.png"), pygame.image.load("player/Player_Sprite2_R.png"),
+                pygame.image.load("player/Player_Sprite3_R.png"), pygame.image.load("player/Player_Sprite4_R.png"),
+                pygame.image.load("player/Player_Sprite5_R.png"), pygame.image.load("player/Player_Sprite6_R.png"),
+                pygame.image.load("player/Player_Sprite_R.png")]
     # move animation for the left
-    move_ani_L = [pygame.image.load("Player_Sprite_L.png"), pygame.image.load("Player_Sprite2_L.png"),
-                pygame.image.load("Player_Sprite3_L.png"), pygame.image.load("Player_Sprite4_L.png"),
-                pygame.image.load("Player_Sprite5_L.png"), pygame.image.load("Player_Sprite6_L.png"),
-                pygame.image.load("Player_Sprite_L.png")]
-
+    move_ani_L = [pygame.image.load("player/Player_Sprite_L.png"), pygame.image.load("player/Player_Sprite2_L.png"),
+                pygame.image.load("player/Player_Sprite3_L.png"), pygame.image.load("player/Player_Sprite4_L.png"),
+                pygame.image.load("player/Player_Sprite5_L.png"), pygame.image.load("player/Player_Sprite6_L.png"),
+                pygame.image.load("player/Player_Sprite_L.png")]
     # Attack animation for the RIGHT
-    attack_ani_R = [pygame.image.load("Player_Sprite_R.png"), pygame.image.load("Player_Attack_R.png"),
-                    pygame.image.load("Player_Attack2_R.png"), pygame.image.load("Player_Attack2_R.png"),
-                    pygame.image.load("Player_Attack3_R.png"), pygame.image.load("Player_Attack3_R.png"),
-                    pygame.image.load("Player_Attack4_R.png"), pygame.image.load("Player_Attack4_R.png"),
-                    pygame.image.load("Player_Attack5_R.png"), pygame.image.load("Player_Attack5_R.png"),
-                    pygame.image.load("Player_Sprite_R.png")]
-
+    attack_ani_R = [pygame.image.load("player/Player_Sprite_R.png"), pygame.image.load("player/Player_Attack_R.png"),
+                    pygame.image.load("player/Player_Attack2_R.png"), pygame.image.load("player/Player_Attack2_R.png"),
+                    pygame.image.load("player/Player_Attack3_R.png"), pygame.image.load("player/Player_Attack3_R.png"),
+                    pygame.image.load("player/Player_Attack4_R.png"), pygame.image.load("player/Player_Attack4_R.png"),
+                    pygame.image.load("player/Player_Attack5_R.png"), pygame.image.load("player/Player_Attack5_R.png"),
+                    pygame.image.load("player/Player_Sprite_R.png")]
     # Attack animation for the LEFT
-    attack_ani_L = [pygame.image.load("Player_Sprite_L.png"), pygame.image.load("Player_Attack_L.png"),
-                    pygame.image.load("Player_Attack2_L.png"), pygame.image.load("Player_Attack2_L.png"),
-                    pygame.image.load("Player_Attack3_L.png"), pygame.image.load("Player_Attack3_L.png"),
-                    pygame.image.load("Player_Attack4_L.png"), pygame.image.load("Player_Attack4_L.png"),
-                    pygame.image.load("Player_Attack5_L.png"), pygame.image.load("Player_Attack5_L.png"),
-                    pygame.image.load("Player_Sprite_L.png")]
+    attack_ani_L = [pygame.image.load("player/Player_Sprite_L.png"), pygame.image.load("player/Player_Attack_L.png"),
+                    pygame.image.load("player/Player_Attack2_L.png"), pygame.image.load("player/Player_Attack2_L.png"),
+                    pygame.image.load("player/Player_Attack3_L.png"), pygame.image.load("player/Player_Attack3_L.png"),
+                    pygame.image.load("player/Player_Attack4_L.png"), pygame.image.load("player/Player_Attack4_L.png"),
+                    pygame.image.load("player/Player_Attack5_L.png"), pygame.image.load("player/Player_Attack5_L.png"),
+                    pygame.image.load("player/Player_Sprite_L.png")]
 
     def __init__(self):
         super().__init__()
-        self.image = pygame.image.load("Player_Sprite_R.png")
-        self.rect = self.image.get_rect(topleft=(W/2, 265))
-        self.position = Vector2(W/2, 265)
+        self.image = pygame.image.load("player/Player_Sprite_R.png")
+        self.rect = self.image.get_rect(topleft=(W/2, 310))
+        self.position = Vector2(W/2, 310)
         self.velocity = Vector2(0, 0)
         self.direction = True
         self.moving = True
@@ -106,10 +113,10 @@ class Player(pygame.sprite.Sprite):
         # calculate the velocity
         self.velocity += self.acceleration
         self.velocity.x *= 0.95
-
         self.position += self.velocity
         self.rect.topleft = self.position
 
+        # taken from https://coderslegacy.com/python/pygame-rpg-player-movement/
         # keep player inside the screen
         if self.position.x > W:
             self.position.x = 0
@@ -117,11 +124,11 @@ class Player(pygame.sprite.Sprite):
             self.position.x = W
 
     def move(self):
-        # reset move frame
+        # if move_frame out of range, restart it from 0
         if self.move_frame > 6:
             self.move_frame = 0
             
-        # move player to next frame
+        # if player has some extent velocity, change the image as running images
         if self.velocity.x > 0.2:
             self.image = self.move_ani_R[self.move_frame]
             self.direction = True
@@ -138,10 +145,11 @@ class Player(pygame.sprite.Sprite):
 
     def attack(self):
         # taken from https://coderslegacy.com/python/pygame-rpg-attack-animations/
+        #restart the attack_frame number if it is out of range
         if self.attack_frame > 10:
             self.attack_frame = 0
             self.attacking = False
-        # check direction for good display
+        # check direction for right display
         if self.direction:
             self.image = self.attack_ani_R[self.attack_frame]
         else:
@@ -156,142 +164,163 @@ class Player(pygame.sprite.Sprite):
             self.health -= 1
             pygame.time.set_timer(game.hitpause_event, 1000)
 
+        # after losing all the health, redraw game sprites, update dispaly, wait 2s then call the gameover menu
         if self.health <= 0:
             self.kill()
+            game.sprites.draw(game.surface)
+            pygame.display.update()
             menu.gameover_menu()
-       
+
+    # update position, movement and attack
     def update(self):
         self.get_pos()
         self.move()
         if self.attacking == True:
             self.attack()
 
-    def respawn(self):
-        game.sprites.add(self)
-        game.playergroup.add(self)
-        self.health = 5
-        for sprite in game.sprites:
-            if type(sprite) is Enemy:
-                sprite.kill() 
-
+# enemy class
 class Enemy(pygame.sprite.Sprite):
     def __init__(self):
-        super().__init__()
-        self.image = pygame.image.load("Enemy.png")
-        self.rect = self.image.get_rect(topleft=(W-30, 255))
-        self.position = Vector2(W-30, 255)
-        self.velocity = Vector2(3, 0)
-        self.direction = True
+        super().__init__()        
+        self.image = pygame.image.load("enemy/Enemy.png")
+        self.rect = self.image.get_rect()
+        self.velocity = Vector2(random.randint(1, 2), 0)
+        self.direction = random.randint(0, 1)
+        if self.direction:
+            self.position = Vector2(W-30, 300)
+        else:
+            self.position = Vector2(0, 300)
 
     def move(self):
-        #verify if position is out of range
-        if self.position.x < 0:
-            self.position.x = 0
-            self.direction = False
-        elif self.position.x > W-30:
-            self.position.x = W-30
-            self.direction = True
-
+        # calculate the position according to the direction
         if self.direction:
             self.position.x -= self.velocity.x
         else:
             self.position.x += self.velocity.x
         self.rect.topleft = self.position
+
+        # when position is out of border, change direction
+        if self.position.x <= 0:
+            self.direction = 0
+        elif self.position.x >= W-30:
+            self.direction = 1
     
     def hit(self):
+        # check if hit
         hit = pygame.sprite.spritecollide(self, game.playergroup, False)
+
+        # when player is attacking and hit, kill enemy, otherwise call player.hit() which will decrease player's health
         if hit and game.player.attacking == True:
             self.kill()
             game.dead_enemy_count += 1
-        if game.dead_enemy_count == game.enemy_num:
-            menu.gamewin_menu()
         if hit and game.player.attacking == False:
             game.player.hit()
+        
+        # if all the enemies are dead, call gamewin menu
+        if game.dead_enemy_count == game.enemy_num:
+            game.sprites.draw(game.surface)
+            pygame.display.update()
+            menu.gamewin_menu()
 
+    # update enemy move and hit
     def update(self):
         self.move()
         self.hit()
 
+# class Game, to handle all the sprites instances and events, set 3 levels and update all
 class Game:
     def __init__(self):
         pygame.init()
-        self.clock = pygame.time.Clock()
-        self.surface = pygame.display.set_mode((W, H))
+        self.clock = pygame.time.Clock()        
         self.sprites = pygame.sprite.LayeredUpdates()
         self.player = Player()
         self.playergroup = pygame.sprite.GroupSingle(self.player)
-        self.ground = Ground()
-        self.groundgroup = pygame.sprite.GroupSingle(self.ground)
         self.healthbar = HealthBar()
         self.background = Background()
-        self.sprites.add(self.background, self.ground, self.player, self.healthbar)
+        self.sprites.add(self.background, self.player, self.healthbar)
         self.run_game = True
         self.enemycreate_event = pygame.USEREVENT + 1
         self.hitpause_event = pygame.USEREVENT + 2
     
+    # initial the game window and keep runing the scene updates
     def start_game(self):
+        self.surface = pygame.display.set_mode((W, H))
         while self.run_game:
             self.update_scene()
 
+    # draw the sprites, call all the sprites update methods, update events handle
     def update_scene(self):
         self.clock.tick(60)
-        self.healthbar.image = self.healthbar.health_ani[self.player.health]
-
         self.event_handle()
         self.sprites.draw(self.surface)
         self.sprites.update()
         pygame.display.update()
 
     def event_handle(self):
+        # get pygame events
         for event in pygame.event.get():
+            # press window close button, quit game
             if event.type == pygame.QUIT:
                 self.run_game = False
             if event.type == KEYDOWN:
+                # press key A to perform player attack
                 if event.key == pygame.K_a:
                     if not self.player.attacking:
                         self.player.attack()
-                        self.player.attacking = True 
+                        self.player.attacking = True
+            # in every second check enemycreate event, then instancer enemy
             if event.type == self.enemycreate_event:
                 if self.enemy_count < self.enemy_num:
                     self.sprites.add(Enemy())
                     self.enemy_count += 1
+            # avoid hit continually
             if event.type == self.hitpause_event:
                 self.player.hitpause = False
+    
+    # have to kill all the sprites in the precedent level, then add sprites for next level, also reset healthbar
+    def respawn(self):
+        for sprite in self.sprites:
+            if type(sprite) is Enemy:
+                sprite.kill() 
+        self.sprites.add(self.player)
+        self.playergroup.add(self.player)
+        self.player.health = 5
 
+    # when called, destroy tkinter menu, set enemycreate interval, set level 1 initial values
     def level1(self):
         menu.root.destroy()        
-        pygame.time.set_timer(self.enemycreate_event, 1200)
-        self.background.image = pygame.image.load("background.png")
-        self.ground.image = pygame.image.load("ground.png")
-        self.level = 1
-        self.enemy_num = 5
-        self.enemy_count = 0
-        self.dead_enemy_count = 0
-
-    def level2(self):
-        menu.root.destroy()
         pygame.time.set_timer(self.enemycreate_event, 1000)
-        self.background.image = pygame.image.load("desert.jpg")
-        self.ground.image = pygame.image.load("desert_ground.png")
-        self.level = 2
+        self.background.image = pygame.image.load("background/background.jpg")
+        self.level = 1
         self.enemy_num = 8
         self.enemy_count = 0
         self.dead_enemy_count = 0
 
-    def level3(self):
+    # when called, destroy tkinter menu, set enemycreate interval, set level 2 initial values
+    def level2(self):
         menu.root.destroy()
         pygame.time.set_timer(self.enemycreate_event, 800)
-        self.background.image = pygame.image.load("dark.png")
-        self.ground.image = pygame.image.load("darkground.png")
-        self.level = 3
+        self.background.image = pygame.image.load("background/desert.jpg")
+        self.level = 2
         self.enemy_num = 12
+        self.enemy_count = 0
+        self.dead_enemy_count = 0
+
+    # when called, destroy tkinter menu, set enemycreate interval, set level 2 initial values
+    def level3(self):
+        menu.root.destroy()
+        pygame.time.set_timer(self.enemycreate_event, 600)
+        self.background.image = pygame.image.load("background/dark.jpg")
+        self.level = 3
+        self.enemy_num = 18
         self.enemy_count = 0
         self.dead_enemy_count = 0
 
 class Menu:
     def __init__(self):
-        self.tk_init()        
+        self.tk_init()    
 
+    # set tkinter menu in the center of screen
     def tk_init(self):
         self.windowWidth = 250
         self.windowHeight = 300
@@ -300,9 +329,9 @@ class Menu:
         self.topleft_y = int(self.root.winfo_screenheight()/2 - self.windowHeight/2)
         self.root.geometry(f"{self.windowWidth}x{self.windowHeight}+{self.topleft_x}+{self.topleft_y}")
 
-    def start_menu(self):
+    def start_menu(self): 
         # get value from radio button (level1, level2 and level3)
-        def select():
+        def select():           
             s = var.get()
             if s == 1:
                 game.level1()
@@ -332,14 +361,14 @@ class Menu:
         label3.pack(pady=5)
         bouton1.pack(side=LEFT)
         bouton2.pack(side=RIGHT)
-
         self.root.mainloop()
 
     def gameover_menu(self):
+        # in waiting time, pause the enemy create event to avoid create enemy before menu selection
         pygame.time.set_timer(game.enemycreate_event, 0)
         self.tk_init()
         def select():
-            game.player.respawn()
+            game.respawn()
             if game.level == 1:
                 game.level1()
             if game.level == 2:
@@ -357,10 +386,12 @@ class Menu:
         self.root.mainloop()
 
     def gamewin_menu(self):
+        # in waiting time, pause the enemy create event to avoid create enemy before menu selection
         pygame.time.set_timer(game.enemycreate_event, 0)
         self.tk_init()
         if game.level == 3:
-            game.player.respawn()
+            game.respawn()
+            # destroy previous menu, reinitialize start menu
             def handle():
                 self.root.destroy()
                 self.tk_init()
@@ -369,17 +400,19 @@ class Menu:
             bouton1 = Button(self.root, text="Main menu", font=("Arial", 15), background="light blue", width=10,
                             command=handle)               
         elif game.level == 1:
+            # go to next level, redraw the sprites
             def handle():
                 game.level2()
-                game.player.respawn()
+                game.respawn()
             label1 = Label(self.root, text="You won level 1", font=("Arial", 25))
         
             bouton1 = Button(self.root, text="Level 2", font=("Arial", 15), background="light blue", width=10,
                             command=handle) 
         elif game.level == 2:
+            # go to next level, redraw the sprites
             def handle():
                 game.level3()
-                game.player.respawn()
+                game.respawn()
             label1 = Label(self.root, text="You won level 2", font=("Arial", 25))
             bouton1 = Button(self.root, text="Level 3", font=("Arial", 15), background="light blue", width=10,
                             command=handle) 
@@ -388,12 +421,10 @@ class Menu:
         label1.pack()
         bouton1.place(x=65, y=100)
         bouton2.place(x=65, y=175)
-
         self.root.mainloop()
 
 W = 700
-H = 350
-
+H = 400
 game = Game()
 menu = Menu()
 menu.start_menu()
